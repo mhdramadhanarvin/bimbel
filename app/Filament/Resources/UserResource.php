b688a2940e444b385db822d7b50da777e6cb005e
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2025-07-23 12:27:26 +0700

    fix: remove unsed

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -4,7 +4,6 @@ namespace App\Filament\Resources;
 
 use App\Enums\UserPaymentStatusEnum;
 use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms\Form;
 use Filament\Infolists\Components\Actions\Action as ActionInfolist;
@@ -16,10 +15,8 @@ use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Filament\Forms;
 use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class UserResource extends Resource
 {
